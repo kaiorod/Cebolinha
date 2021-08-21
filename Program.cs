eba9de8ca1ef84c5e6926fec616738d85166979a
kaiorod <caiorodrigo2775@gmail.com>
2021-08-21 14:25:36 -0300

    finzaliza Cebolinha

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string texto;
+            Console.Clear();
+            Console.Beep();
+
+            Console.Write("Digite o texto : ");
+            texto = Console.ReadLine().Replace("r", "l").Replace("R","L");
+
+            Console.Beep();
+            Console.Write("\nComo diz o cebolinha : ");
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine(texto);
+
+            Console.ResetColor();
+            
+        
+        }
+    }
+}
